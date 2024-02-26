ebd37ebbb546456fbcfd7c8f2b10e8b98d9b9db3
rafaeljosecomputacao <132665090+rafaeljosecomputacao@users.noreply.github.com>
2024-02-26 14:00:11 -0300

    update-javascript

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const $heading = document.querySelector(".carousel-caption h1");
 
 const $description = document.querySelector(".carousel-caption p");
 
-// Vectors
+// Vectors with the data of each slide
 const $images = [
     "banner-1.jpg",
     "banner-2.jpg",
@@ -31,20 +31,17 @@ const $descriptions = [
     "New interactive features"
 ];
 
-// Radios
+// Radios to show the current slide
 const $radio1 = document.getElementById("radio-1");
-
 const $radio2 = document.getElementById("radio-2");
-
 const $radio3 = document.getElementById("radio-3");
-
 const $radio4 = document.getElementById("radio-4");
 
 // Vector index
 let id = 0;
 
-// Function to exchange data on the slide
-function slide(id) {
+// Function to change slide data
+function changeDataSlide(id) {
     // Change image
     $carousel.style.backgroundImage = `url(../img/${$images[id]})`;
     // Change title
@@ -53,7 +50,7 @@ function slide(id) {
     $description.innerHTML = $descriptions[id];
 }
 
-// Left button function
+// Function to change the slide by clicking on the left arrow
 $carouselArrowLeft.addEventListener("click", () => {
     id--;
 
@@ -61,11 +58,11 @@ $carouselArrowLeft.addEventListener("click", () => {
         id = $images.length - 1;
     }
 
-    slide(id);
+    changeDataSlide(id);
     document.getElementById("radio-" + (id + 1)).checked = true;
 });
 
-// Right button function
+// Function to change the slide by clicking on the right arrow
 $carouselArrowRight.addEventListener("click", () => {
     id++;
     
@@ -73,17 +70,19 @@ $carouselArrowRight.addEventListener("click", () => {
         id = 0;
     }
 
-    slide(id);
+    changeDataSlide(id);
     document.getElementById("radio-" + (id + 1)).checked = true;
 });
 
 // Function to change the slide by clicking on the radio
-function changeToRadio(radio) {
-    // Vectors start at zero
-    slide(radio - 1);
+function changeByRadio(radio) {
+    // Updating the id
+    id = radio - 1;
+    
+    changeDataSlide(id);
 }
 
-$radio1.addEventListener("click", () => (changeToRadio(1)));
-$radio2.addEventListener("click", () => (changeToRadio(2)));
-$radio3.addEventListener("click", () => (changeToRadio(3)));
-$radio4.addEventListener("click", () => (changeToRadio(4)));
+$radio1.addEventListener("click", () => { changeByRadio(1) });
+$radio2.addEventListener("click", () => { changeByRadio(2) });
+$radio3.addEventListener("click", () => { changeByRadio(3) });
+$radio4.addEventListener("click", () => { changeByRadio(4) });
